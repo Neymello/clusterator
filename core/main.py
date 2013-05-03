e3e19d8322635b959ba2c11d106df25015f379a3
Silveira Neto <silveiraneto@gmail.com>
2013-05-02 20:47:26 -0400

    pprint fix

--- core/main.py
+++ core/main.py
@@ -3,11 +3,7 @@ from util import Util
 from tokenator import Tokenator
 
 # pprint is like print_r from php to python. just use pprint(stuff)
-from pprint import PrettyPrinter
-pp = PrettyPrinter(indent=4)
-def pprint(stuff):
-    pp.pprint(stuff)
-
+from pprint import pprint
 
 if __name__ == '__main__':
     doc = Util().getFileContent("../examples/TEST.DAT")
