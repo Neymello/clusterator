10f77b5553e6b3feece831cb08482ef91aa02ed1
lucastsa <lucas.sa@gmail.com>
2013-05-11 14:52:27 -0400

    added cluster example graphs

--- core/analysis.py
+++ core/analysis.py
@@ -5,9 +5,9 @@ import graph
 import matplotlib.pyplot as plt
 import networkx as nx
 import csv
-from multiprocessing import Pool
 import pickle
 import numpy
+from pprint import pprint
 
 def compute_number_sim_threshold(sim_threshold):
     g = graph.Graph(simm, sim_threshold)
@@ -35,6 +35,35 @@ def compute_size_sim_threshold(sim_threshold):
 
     return [sim_threshold, clustersize_clique, clustersize_single_link, clustersize_star, clustersize_string]
 
+def show_cluster_example(sim_threshold):
+    g = graph.Graph(simm, 150)
+
+    clustersize_clique = [c for c in g.cliques() if len(c) == 8][0]
+    sg = g.nx_graph.subgraph(clustersize_clique)
+    nx.draw_circular(sg, alpha=0.8, node_size=2000, node_color='b')
+    plt.show()
+
+    clustersize_star = [c for c in g.star() if len(c) == 10][0]
+    sg = g.nx_graph.subgraph(clustersize_star)
+    pos = nx.spring_layout(sg)
+    nx.draw(sg, alpha=0.8, node_size=2000, node_color='b', pos=pos)
+    plt.show()
+
+    clustersize_string = g.string()[0]
+    sg = g.nx_graph.subgraph(clustersize_string)
+    nx.draw_random(sg, alpha=0.8, node_size=2000, node_color='b')
+    plt.show()
+
+    #g = graph.Graph(simm, 600)
+
+    clustersize_single_link = []# map(len, g.single_link())
+
+    return [sim_threshold, clustersize_clique, clustersize_single_link, clustersize_star, clustersize_string]
+
+def draw_cluster(g, c):
+    sg = g.nx_graph.subgraph(c)
+    nx.draw_circular(sg, alpha=0.8, node_size=2000, node_color='b')
+    plt.show()
 
 def t(x):
     return x
@@ -62,6 +91,9 @@ if __name__ == "__main__":
     parser.add_argument('--size', action='store_true',
                         help='analysis size of clusters by similarity threshold')
 
+    parser.add_argument('--cluster', action='store_true',
+                        help='show an example of a cluster for each algorithm')
+
     args = parser.parse_args()
 
     if args.number:
@@ -79,6 +111,10 @@ if __name__ == "__main__":
             with open('../analysis/size_by_similarity_threshold.csv', 'a') as f:
                   writer = csv.writer(f)
                   writer.writerow(row)
+
+    elif args.cluster:
+        pprint(show_cluster_example(150))
+
 
     else:
         parser.print_help()
